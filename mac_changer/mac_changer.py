8843898200407b7e4614f88f5a8cacf0be4220fd
David H <davhernandez17@gmail.com>
2020-03-18 21:54:39 -0400

    first commit

---
+++ mac_changer/mac_changer.py
@@ -0,0 +1,61 @@
+#! usr/bin/env/python
+
+import subprocess
+import optparse
+import re
+
+
+def change_mac(new_mac, interface):
+    print("Changing mac address to " + new_mac)
+    subprocess.call(['ifconfig', interface, 'down'])
+    subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+    subprocess.call(['ifconfig', interface, 'up'])
+
+
+def arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Select the interface to change it's MAC address")
+    parser.add_option("-m", "--new_mac", dest="new_mac", help="Select the value for the new MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.new_mac:
+        parser.error("[+] Please specify a MAC Address")
+    elif not options.interface:
+        parser.error("[+] Please specify an interface")
+    else:
+        return options
+
+
+def current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    ifconfig_new_mac_address = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if ifconfig_new_mac_address:
+        return ifconfig_new_mac_address.group(0)
+    else:
+        print("[+]mac address could not be changed")
+
+
+options = arguments()
+
+change_mac(options.new_mac, options.interface)
+
+
+if options.new_mac == current_mac(options.interface):
+    print("The new Mac has been changed to "+str(current_mac(options.interface)))
+
+
+
+
+
+# print ifconfig_result
+
+
+# Comments from the first version
+
+# ifconfig interface down
+# ifconfig interface hw ehter new_interface
+# ifconfig interface up
+
+
+# subprocess.call("ifconfig "+device+" down", shell = "TRUE")
+# subprocess.call("ifconfig "+device+" hw ether "+new_mac, shell = "TRUE")
+# subprocess.call("ifconfig "+device+" up", shell = "TRUE")
---
+++ network_scanner/network_scaner.py
@@ -0,0 +1,23 @@
+import scapy.all as scapy
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    ethernet_frame = broadcast/arp_request
+    print(ethernet_frame.summary())
+    ethernet_frame.show()
+    #scapy.ls(scapy.Ether())
+    #print("=" * 60)
+    #print(broadcast.summary())
+    #print("=" * 60)
+    #scapy.ls(scapy.ARP)
+    #print("=" * 60)
+    #print(arp_request.summary())
+    #print(arp_request.summary())
+    #arp_request.show()
+
+
+ip_range = "10.0.2.1/24"
+
+scan(ip_range)
